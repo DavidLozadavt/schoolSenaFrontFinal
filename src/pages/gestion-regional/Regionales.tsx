426ae4eab1d70512e856a433c38f0a57b768875e
Jorge Porras <jorgeeporrassandoval@gmail.com>
2026-01-21 12:51:40 -0500

    feat(filtro regionales): Se agrega el filtro para la busqueda de las regionales

--- src/pages/gestion-regional/Regionales.tsx
+++ src/pages/gestion-regional/Regionales.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FormularioRegional from './FormularioRegional';
 import ListaRegionales from './ListaRegionales';
+import axios from 'axios';
+import Select from 'react-select';
+
+interface Ciudades {
+  id: number;
+  descripcion: string;
+}
 
 const Regionales = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   //Para actualizar la Data una vez ocurra un vambio:
   const [evento, setEvento] = useState<boolean>(true);
+  const [ciudades, setCiudades] = useState<Ciudades[]>([]);
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await axios.get('ciudades');
+      setCiudades(res.data);
+    };
+    loadData();
+  }, []);
+
+  const options = ciudades.map((val) => ({
+    value: val.id,
+    label: val.descripcion
+  }));
   return (
     <div className="relative z-10 flex flex-col items-center w-full h-screen">
       {/** titulo */}
@@ -25,7 +45,7 @@ const Regionales = () => {
           </div>
           <input
             type="text"
-            placeholder="Buscar programa..."
+            placeholder="Buscar regional..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="w-full px-3 text-sm font-medium transition-opacity bg-transparent border-none outline-none opacity-0 group-hover:opacity-100 focus:opacity-100 dark:text-white"
@@ -46,11 +66,27 @@ const Regionales = () => {
           </span>
         </button>
       </div>
+      <div className="flex items-center justify-between w-full max-w-5xl gap-6 px-4 py-3 mx-auto mb-8 bg-white rounded-lg shadow">
+        {/** Filtros */}
+        <div className="flex flex-col w-64">
+          <label className="mb-2 text-sm font-semibold text-gray-700">Filtro por ciudad</label>
+          <Select
+            options={options}
+            onChange={(e) => setSearchTerm(e?.label as string)}
+            className="text-sm"
+          />
+        </div>
+      </div>
+
       {isModalOpen && (
-        <FormularioRegional isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} setEvento={setEvento}/>
+        <FormularioRegional
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          setEvento={setEvento}
+        />
       )}
-      <div className='p-1'>
-      <ListaRegionales searchTerm={searchTerm} evento={evento} setEvento={setEvento} />
+      <div className="p-1">
+        <ListaRegionales searchTerm={searchTerm} evento={evento} setEvento={setEvento} />
       </div>
     </div>
   );
