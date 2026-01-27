0d1234f9fb62eab415401f7c045d416d0e7f03a2
Jorge Porras <jorgeeporrassandoval@gmail.com>
2026-01-27 16:40:55 -0500

    feat(fichas): Se agregan la visualizacion de las fichas creadas

--- src/pages/gestion-fichas/Fichas.tsx
+++ src/pages/gestion-fichas/Fichas.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FormularioFichasSena from './FormularioFichasSena';
+import ListaFichas from './ListaFichas';
 
 const Fichas: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -52,6 +53,9 @@ const Fichas: React.FC = () => {
           setEvento={setEvento}
         />
       )}
+      <div className="p-1">
+        <ListaFichas searchTerm={searchTerm} evento={evento} setEvento={setEvento} />
+      </div>
     </div>
   );
 };
