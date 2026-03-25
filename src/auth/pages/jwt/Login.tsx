585871e6d3562af7aa573f5eca9db14ad137829f
Carlos Pinza <carlosdanielpinzadiaz@gmail.com>
2026-03-25 18:23:52 -0500

    fix:se agregan ajusten para el correo se escriba en minsiculas y en mayusculas y no se bloquee en mayusculas

--- src/auth/pages/jwt/Login.tsx
+++ src/auth/pages/jwt/Login.tsx
@@ -143,7 +143,8 @@ const Login = () => {
               autoComplete="off"
               type="text"
               {...formik.getFieldProps('email')}
-              data-preserve-case="true"
+              data-preserve-case
+              data-no-uppercase
               className={clsx(
                 'w-full rounded-xl border px-4 py-3 text-sm dark:border-coal-100 bg-white dark:bg-coal-400 outline-none transition-all',
                 'focus:border-orange-500 focus:ring-4 focus:ring-orange-500/20',
