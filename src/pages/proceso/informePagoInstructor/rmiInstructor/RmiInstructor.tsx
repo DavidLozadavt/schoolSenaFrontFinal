4c4b4ced1a0f67a2fca24a6ffe45022dcf5acd90
IngAlim2023 <jorgeeporrassandoval@gmail.com>
2026-04-14 11:55:49 -0500

    Feat(anio): ahora se inicializa los rmi, informes y pago en el año actual en la vista del instructor

--- src/pages/proceso/informePagoInstructor/rmiInstructor/RmiInstructor.tsx
+++ src/pages/proceso/informePagoInstructor/rmiInstructor/RmiInstructor.tsx
@@ -38,7 +38,7 @@ const RmiInstructor: React.FC = () => {
   const authContext = useContext(AuthContext);
   if (!authContext) throw new Error('AuthContext debe usarse dentro de AuthProvider');
 
-  const [anioGestion, setAnioGestion] = useState<number>(0);
+  const [anioGestion, setAnioGestion] = useState<number>(new Date().getFullYear());
   const [aniosContrato, setAniosContrato] = useState<number[]>([]);
   const [dataRmi, setDataRmi] = useState<ContratoRmi[]>([]);
   const [loadingRmi, setLoadingRmi] = useState(false);
